6fb186ee8c44393e83cb68c922d88c9e4dc2cc6c
mahmoud-khalil8 <mahmoudkhalil8.g@gmail.com>
2023-09-18 16:03:01 +0300

    small update

--- src/fetchBreedList.js
+++ src/fetchBreedList.js
@@ -1,7 +1,9 @@
 const fetchPet = async ({ queryKey }) => {
   const animal = queryKey[1];
   if (!animal) return [];
-  const apiRes = await fetch(`http://pets-v2.dev-apis.com/breeds?animal=${id}`);
+  const apiRes = await fetch(
+    `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+  );
   if (!apiRes.ok) {
     throw new Error(`breeds/${animal} fetch not ok`);
   }
